53a7b5a4fdced62dccd6117062c032a652007ec4
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-02-08 17:57:32 +0300

    Update twoDA5.java

--- src/homeWork/massive/twoMassive/twoDA5.java
+++ src/homeWork/massive/twoMassive/twoDA5.java
@@ -2,7 +2,7 @@ package homeWork.massive.twoMassive;
 //Вывести таблицу умножения используя двумерный массив
 
 import java.util.Scanner;
-
+//if i enter 8, then it print 7x7 table, but as a user i expect 8x8
 public class twoDA5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
