9a60051a53440e636e49fec1cd3dfb49ef553a55
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-03-05 10:18:40 +0300

    Update MethodCopyArray.java

--- src/homeWork/method/MethodCopyArray.java
+++ src/homeWork/method/MethodCopyArray.java
@@ -8,19 +8,19 @@ import java.util.Scanner;
 public class MethodCopyArray {
     public static void main(String[] args) {
         Scanner scanner=new Scanner (System.in);
-        int[] array=new int[sizeArray (scanner)];
+        int[] array=new int[initSizeArray (scanner)];
         int[]array2=arrayValue (scanner,array);
         arrayOutPut (array2);
-        arrayCopy (array2);
+        initArrayCopy (array2);
 }
-public static int sizeArray(Scanner scanner) {
+public static int initSizeArray(Scanner scanner) {
     System.out.println ("Enter size");
         int sizeArray=scanner.nextInt ();
         if (sizeArray<0) {
             System.out.println ("It is wrong value. Try again");
         }return sizeArray;
 }
-public static int [] arrayValue(Scanner scanner, int[] array) {
+public static int [] initArrayValue(Scanner scanner, int[] array) {
     System.out.println ("Enter value");
         for (int i=0; i <array.length ; i++) {
         array[i]=scanner.nextInt ();
@@ -29,12 +29,12 @@ public static int [] arrayValue(Scanner scanner, int[] array) {
 static void arrayOutPut (int [] array) {
     System.out.println (Arrays.toString (array));
 }
-static void arrayCopy(int[] array) {
+static void initArrayCopy(int[] array) {
     int [] copyArray=new int[array.length];
         for (int i=0; i <array.length ; i++) {
             copyArray [i]=array[i];
 
-    }System.out.println (Arrays.toString (copyArray));
-
+    }
+    arrayOutPut(copyArray);
 }
 }
