12a4f1783e0735913cb79429b126164cf8515539
Mona Liza <truman5555@yandex.ru>
2022-03-15 22:03:08 +0300

    Commit 2

--- src/homeWork/method/ExamTwo.java
+++ src/homeWork/method/ExamTwo.java
@@ -7,52 +7,53 @@ import java.util.Scanner;
 public class ExamTwo {
     public static void main(String[] args) {
         Scanner scanner=new Scanner (System.in);
-//create it in a place, where you use it
-        Random random=new Random ();
 
         System.out.println ("Choose array to work with:" + "\n" + "1:numbers" + "\n" + "2:strings");
         int choiceOne=scanner.nextInt ();
         if (choiceOne == 2) {
-            String[] arrayString=new String[arrayLength (scanner)];
-            String[] arrayString1=arrayInConsoleString (arrayString, scanner);
-            outPutArrayString (arrayString1);
-            sortArrayString (arrayString1);
-//did not find second String task with random
-//number replacement. Do it later. 
+            String[] arrayString=new String[findeArrayLength (scanner)];
+            String[] arrayString1=initArrayConsoleString (arrayString, scanner);
+            System.out.println ("Origin array: " + Arrays.toString (arrayString1));
+            Arrays.sort (arrayString1);
+            System.out.println ("Sorted array: " + Arrays.toString (arrayString1));
+            replaсeLetter (arrayString1, scanner);
+
         } else {
             System.out.println ("How do you want to initialize array?" + "\n" + "1: from console" + "\n" +
                     "2: random numbers");
             int choicetwo=scanner.nextInt ();
-            int[] array=new int[arrayLength (scanner)];
-//initialise array and then perform all of the operations. Do not duplicate your code. Do your actions after
-//if-else. You will do it anyway, no matter 
-//what condition will be true. 
+            int[] array=new int[findeArrayLength (scanner)];
+
             if (choicetwo == 1) {
-                int[] arrayConsole=arrayInConsole (scanner, array);
-                outPutArray (arrayConsole);
-                determinMaxMin (arrayConsole);
-                sorting (arrayConsole);
+                int[] arrayConsole=initConsoleArray (scanner, array);
+                System.out.println ("Origin array: " + Arrays.toString (arrayConsole));
+                determinMax (arrayConsole);
+                determinMin (arrayConsole);
+                sort (arrayConsole);
+                System.out.println ("Sorted array: " + Arrays.toString (arrayConsole));
                 findIndex (arrayConsole, scanner);
             } else {
-                int[] arrayRandom=arrayInRandom (random, array);
-                outPutArray (arrayRandom);
-                determinMaxMin (arrayRandom);
-                sorting (arrayRandom);
+                int[] arrayRandom=initRandomArray (array);
+                System.out.println ("Origin array: " + Arrays.toString (arrayRandom));
+                determinMax (arrayRandom);
+                determinMin (arrayRandom);
+                sort (arrayRandom);
+                System.out.println ("Sorted array: " + Arrays.toString (arrayRandom));
                 findIndex (arrayRandom, scanner);
             }
-
         }
-
     }
-//initRandomArray
-    static int[] arrayInRandom(Random random, int[] array) {
+
+    static int[] initRandomArray(int[] array) {
+        Random random=new Random ();
+        int randomNumber=random.nextInt (10) + 1;
         for (int i=0; i < array.length; i++) {
-            array[i]=random.nextInt (10) + 1;
+            array[i]=randomNumber;
         }
         return array;
     }
-//initConsoleArray
-    static int[] arrayInConsole(Scanner scanner, int[] array) {
+
+    static int[] initConsoleArray(Scanner scanner, int[] array) {
         System.out.println ("Enter value:");
         for (int i=0; i < array.length; i++) {
             array[i]=scanner.nextInt ();
@@ -60,38 +61,35 @@ public class ExamTwo {
         return array;
     }
 
-    static void outPutArray(int[] array) {
-        System.out.println ("Origin array: " + Arrays.toString (array));
-    }
-outPutArray. Overloaded methods!!
-    static void outPutArrayString(String[] array) {
-        System.out.println ("Origin array: " + Arrays.toString (array));
-    }
-//findArrayLength
-    static int arrayLength(Scanner scanner) {
+    static int findeArrayLength(Scanner scanner) {
         System.out.println ("Enter length:");
         int length=scanner.nextInt ();
         if (length < 0) {
             System.out.println ("Try again:");
-            return arrayLength (scanner);
+            return findeArrayLength (scanner);
         }
         return length;
     }
-//It should be placed in 2 separate methods: findMax
-//findMin. And return this values. Do not print in here. 
-    static void determinMaxMin(int array[]) {
+
+    static void determinMax(int array[]) {
         int max=array[0];
-        int min=array[0];
         for (int i=0; i < array.length; i++) {
             if (max < array[i])
                 max=array[i];
+        }
+        System.out.println ("Max: " + max);
+    }
+
+    static void determinMin(int array[]) {
+        int min=array[0];
+        for (int i=0; i < array.length; i++) {
             if (min > array[i])
                 min=array[i];
         }
-        System.out.println ("Min: " + min + "\n" + "Max: " + max);
+        System.out.println ("Min: " + min);
     }
-//sort not sortING
-    static void sorting(int[] array) {
+
+    static void sort(int[] array) {
         int k=0;
         int temp=0;
         while (k < array.length) {
@@ -104,9 +102,6 @@ outPutArray. Overloaded methods!!
             }
             k++;
         }
-//do not print it here. Look at your method name
-//does it tell me that it is printing something?
-        System.out.println (Arrays.toString (array));
     }
 
     static int findIndex(int[] array, Scanner scanner) {
@@ -118,8 +113,8 @@ outPutArray. Overloaded methods!!
         }
         return -1;
     }
-//initArrayConsoleString
-    static String[] arrayInConsoleString(String[] array, Scanner scanner) {
+
+    static String[] initArrayConsoleString(String[] array, Scanner scanner) {
         System.out.println ("Enter words:");
         for (int i=0; i < array.length; i++) {
             array[i]=scanner.next ();
@@ -127,13 +122,18 @@ outPutArray. Overloaded methods!!
         return array;
     }
 
-    static void sortArrayString(String[] array) {
-        Arrays.sort (array);
-//print in in another place. You have method for sorting
-:/make it overloaded!!!
-        System.out.println ("Sorted array: " + Arrays.toString (array));
-    }
+    static String[] replaсeLetter(String[] array, Scanner scanner) {
+        System.out.println ("Enter letter you want to replace: ");
+        String letter=scanner.next ();
+        Random random=new Random ();
+        int randomNumber=random.nextInt ();
+        for (int i=0; i < array.length; i++) {
+            array[i]=array[i].replace (letter, String.valueOf (randomNumber));
+            System.out.println (array[i]);
+        }
+        return array;
 
+    }
 }
 
 
