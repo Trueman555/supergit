fb3226c189af2806d1183e651a29222935c48802
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-03-27 07:50:20 +0300

    Update Vegetables.java

--- src/homeWork/oop/oopClassWork/Salad/Vegetables.java
+++ src/homeWork/oop/oopClassWork/Salad/Vegetables.java
@@ -3,6 +3,7 @@ package homeWork.oop.oopClassWork.Salad;
 abstract public class Vegetables {
     private int calories;
     private int weight;
+    //you can get it from toy class names - Tomato... as getClass().getSimpleName()
     private String name;
 
     public Vegetables(int calories, int weight, String name) {
@@ -22,5 +23,6 @@ abstract public class Vegetables {
     public String getName() {
         return name;
     }
+    //void->double or int! No printing in such methods. What if i will need this value in further? How will I get it?
    abstract public void countCalories ();
 }
